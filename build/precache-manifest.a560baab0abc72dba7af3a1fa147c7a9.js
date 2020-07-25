6f082d69cc9d09ca83026bfb455d9786b05269cb
iKurum <ikurum@yeah.net>
2020-07-25 22:10:36 +0800

    '打包测试'

---
+++ build/precache-manifest.a560baab0abc72dba7af3a1fa147c7a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8899f60bb1a74eec1aa5dbf6cfd14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "626ba080ad0c33d1dc51",
+    "url": "/static/css/main.0917306a.chunk.css"
+  },
+  {
+    "revision": "a23ebe9f34fdbf70b688",
+    "url": "/static/js/2.63e3f348.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63e3f348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626ba080ad0c33d1dc51",
+    "url": "/static/js/main.08eebbac.chunk.js"
+  },
+  {
+    "revision": "78a57674cd14bdab208e",
+    "url": "/static/js/runtime-main.2289431b.js"
+  },
+  {
+    "revision": "1d2fc2b60502e5a045bcd2a5b8b3f6d6",
+    "url": "/static/media/1.1d2fc2b6.jpeg"
+  },
+  {
+    "revision": "bf5a7a31c5c54b774cda8bf298ffb4f9",
+    "url": "/static/media/2.bf5a7a31.jpeg"
+  },
+  {
+    "revision": "e2d0ac986c149485b2aef46c6c8498d3",
+    "url": "/static/media/3.e2d0ac98.png"
+  },
+  {
+    "revision": "3737b0ac788e86588d5cf6abd49c87e9",
+    "url": "/static/media/4.3737b0ac.png"
+  }
+]);
