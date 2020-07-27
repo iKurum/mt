ca1107e958857132feda1424b262c2bc349f47c4
ikurum <ikurum.zafkiel@gmail.com>
2020-07-27 17:45:40 +0800

    '调整border'

--- precache-manifest.bf44c125b655561be01f232f62a02cdd.js
+++ precache-manifest.bf44c125b655561be01f232f62a02cdd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c39e48aad6a9b6af1851943a14a9d33",
+    "revision": "bcec66b13fb9c1d8af90b4541394cb14",
     "url": "./index.html"
   },
   {
-    "revision": "5dd23d1c11cec600d753",
-    "url": "./static/css/main.5ca12547.chunk.css"
+    "revision": "f946a1ee96483d4ba1cb",
+    "url": "./static/css/main.5431b246.chunk.css"
   },
   {
     "revision": "a23ebe9f34fdbf70b688",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.63e3f348.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5dd23d1c11cec600d753",
-    "url": "./static/js/main.54ea5a7d.chunk.js"
+    "revision": "f946a1ee96483d4ba1cb",
+    "url": "./static/js/main.076b49ad.chunk.js"
   },
   {
     "revision": "3e9302ef298c97d775e0",
